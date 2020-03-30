d37552ba54a56037fa76958de2e3a567853f3114
JJV <jacksonjosephv@gmail.com>
2020-03-30 17:58:48 +1100

    the search bar

--- application/views/carshare_search.php
+++ application/views/carshare_search.php
@@ -4,6 +4,19 @@ $data['title'] = ucfirst('search');
 $this->load->view('inc/header', $data);
 
 ?>
+<!--== Pick Up Location ==-->
+
+   <!--== Pick Up Date ==-->
+   <div class="pick-up-date book-item">
+    <h4>PICK-UP DATE:</h4>
+    <input id="startDate" placeholder="Pick Up Date" />
+
+   <div class="return-car">
+   <h4>Return DATE:</h4>
+   <input id="endDate" placeholder="Return Date" />
+    </div>
+    </div>
+    <!--== Pick Up Location ==-->
 
 
 
