e98ddc9f36ae966b6051d8800c877e4dfb5f2b03
Shehan Wences <s3757847@student.rmit.edu.au>
2020-03-30 19:33:37 +1100

    controller edit 2

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'welcome';
-$route['search'] = 'search';
+$route['search'] = 'welcome/search';
 $route['404_override'] = '';
 
