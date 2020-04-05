7eda195265b0772335fd181530609e0992602948
Shehan Wences <s3757847@student.rmit.edu.au>
2020-04-05 14:27:12 +1000

    Reset Password 5

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -185,6 +185,8 @@ class Welcome extends CI_Controller {
 
 		}
 		if (($this->input->server('REQUEST_METHOD')) == 'POST') {
+			
+			$this->load->model('carshare_model');
            
 			if(!preg_match("/^(([^<>()\[\]\\.,;:\s@']+(\.[^<>()\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i", $_POST['Email'])){
 				echo "Invalid Email";
