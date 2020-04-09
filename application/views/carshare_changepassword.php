ab20a1881ebaaff7911b54f2ff7c953fb8a50b8e
Shehan Wences <s3757847@student.rmit.edu.au>
2020-04-09 15:38:28 +1000

    password change controller and view change 3

--- application/views/carshare_changepassword.php
+++ application/views/carshare_changepassword.php
@@ -36,7 +36,7 @@ $this->load->view('inc/header', $data);
 	<div class="contact-page-wrao section-padding">
         <div class="container">
             <div class="row">
-                <div class="col-lg-10 m-auto">
+                <div class="col-lg-6 m-auto">
 				<h1 class="font-weight-normal text-center">Change Password</h1>
 				<?php if(isset($passerror)){   ?>
 							<div class="alert alert-danger">
@@ -51,7 +51,7 @@ $this->load->view('inc/header', $data);
                     <div class="contact-form">
                         <form action="<?php echo base_url('/passwordchange'); ?>" method="post" >
                             <div class="row">
-                                <div class="col-lg-6 col-md-6">
+                                <div class="col-lg-12 col-md-12">
                                     <div class="name-input">
                                         <input name="newpass" type="password" placeholder="New Password">
                                     </div>
@@ -60,8 +60,8 @@ $this->load->view('inc/header', $data);
                             </div>
 
                             <div class="row">
-                                <div class="col-lg-6 col-md-6">
-                                    <div class="name-input">
+                                <div class="col-lg-12 col-md-12">
+                                    <div class="website-input">
                                         <input name="confirmpass" type="password" placeholder="New Password again">
                                     </div>
                                 </div>
