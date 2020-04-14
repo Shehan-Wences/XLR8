fede2c89841d51ec3891e0c0e3d9ad265a38108d
Shehan Wences <s3757847@student.rmit.edu.au>
2020-04-14 16:03:31 +1000

    Error Page

--- application/views/errors/cli/error_404.php
+++ application/views/errors/cli/error_404.php
@@ -1,8 +1,26 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
-echo "\nERROR: ",
-	$heading,
-	"\n\n",
-	$message,
-	"\n\n";
+$data['title'] = ucfirst('oops');
+$this->load->view('inc/header', $data);
+?>
+    <section id="page-404-wrap" class="section-padding overlay">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="page-404-content">
+                    	<div class="number">
+							<div class="four">4</div>
+							<div class="zero">
+								<img src="assets/img/404.png" alt="JSOFT">
+							</div>
+							<div class="four">4</div>
+                    	</div>
+                    	<h4>PAGE NOT FOUND !</h4>
+                    	<p>SORRY, WE COULDN'T FIND THE PAGE <br> YOU'RE LOOKING.</p>
+                    	
+                    </div>
+                </div>
+        	</div>
+        </div>
+    </section>
+<?php $this->load->view('inc/footer'); ?>
