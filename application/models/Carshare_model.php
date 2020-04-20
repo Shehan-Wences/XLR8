a85657aedb2a7dc62bc3068011047b439df02e02
nicholas shepherd <32504617+Nickunel@users.noreply.github.com>
2020-04-20 12:53:00 +1000

    Update Carshare_model.php
    added cardetail()

--- application/models/Carshare_model.php
+++ application/models/Carshare_model.php
@@ -48,6 +48,25 @@ class carshare_model extends CI_Model {
         }
         return $data;
     }
+	
+    public function member_login_details($email, $pass) {
+        $data = array();
+        $this->db->select('Email');
+        $this->db->select('Fname');
+		$this->db->select('Lname');
+        $this->db->select('Password');
+		$this->db->select('Status');
+        $this->db->from('customer');
+		$this->db->where("customer.Email", $email);
+        $this->db->where("customer.Password", sha1($pass));
+
+        $open_list = $this->db->get();
+		
+        foreach ($open_list->result() as $open_info) {
+            $data[] = $open_info;
+        }
+        return $data;
+    }
 
     public function member_login_details($email, $pass) {
         $data = array();
