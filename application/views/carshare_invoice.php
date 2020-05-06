e4d74f8366758f807e36e41875d2db50d1bc828e
JJV <jacksonjosephv@gmail.com>
2020-05-06 22:58:36 +1000

    invoice fixed

--- application/views/carshare_invoice.php
+++ application/views/carshare_invoice.php
@@ -4,7 +4,100 @@ $data['title'] = ucfirst('invoice');
 $this->load->view('inc/header', $data);
 
 ?>
- 
+ <style>
+ /*==============================
+    Invoice Page Css
+================================*/
+.clearfix:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+a {
+  color: #5D6975;
+  text-decoration: underline;
+}
+
+header {
+  padding: 10px 0;
+  margin-bottom: 30px;
+}
+
+#logo {
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+#logo img {
+  width: 90px;
+}
+
+h1 {
+  border-top: 1px solid  #5D6975;
+  border-bottom: 1px solid  #5D6975;
+  color: #5D6975;
+  font-size: 2.4em;
+  line-height: 1.4em;
+  font-weight: normal;
+  text-align: center;
+  margin: 0 0 20px 0;
+  background: url(dimension.png);
+}
+
+#project {
+  float: left;
+}
+
+#project span {
+  color: #5D6975;
+  text-align: right;
+  width: 52px;
+  margin-right: 10px;
+  display: inline-block;
+  font-size: 0.8em;
+}
+
+#company {
+  float: right;
+  text-align: right;
+}
+
+#project div,
+#company div {
+  white-space: nowrap;        
+}
+table {
+  border-collapse: collapse;
+  width: 100%;
+  position:relative;
+  bottom:70px;
+}
+
+th, td {
+  padding: 8px;
+  text-align: center;
+  border-bottom: 1px solid #ddd;
+}
+
+th {
+  background-color: #4da4bd;
+  color: white;
+}
+ .company{
+  text-align: right;
+  position: relative;
+  right: 50px;
+ }
+ .project{
+  text-align: left;
+  position: relative;
+  left: 30px;
+  bottom: 110px;
+ }
+
+</style>
+
  <!DOCTYPE html>
  <section id="page-title-area" class="section-padding overlay">
         <div class="container">
@@ -30,13 +123,13 @@ $this->load->view('inc/header', $data);
   <body>
    
      
-      <div id="company" class="clearfix">
+      <div class="company" class="clearfix">
         <div>XLR8</div>
         <div>45 Collins St, Melbourne<br />  VIC 3000, AU</div>
         <div>(602) 519-0450</div>
         <div><a href="mailto:company@example.com">xlr8-rental@gmail.com</a></div>
       </div>
-      <div id="project">
+      <div class="project">
         <div><span>PROJECT</span> Car Rental</div>
         <div><span>CLIENT</span> Name</div>
         <div><span>ADDRESS</span> 79 victoria street, VIC 3000, AU</div>
@@ -58,22 +151,22 @@ $this->load->view('inc/header', $data);
         </thead>
         <tbody>
           <tr>
-            <td class="service">Design</td>
-            <td class="desc"> 2017 Lexus IS200T F Sport (Car Hired) </td>
-            <td class="unit">$0.00</td>
-            <td class="total">$</td>
+            <td> Design</td>
+            <td> 2017 Lexus IS200T F Sport (Car Hired) </td>
+            <td >$0.00</td>
+            <td>$</td>
           </tr>
           <tr>
             <td colspan="3">SUBTOTAL</td>
-            <td class="total">$</td>
+            <td >$</td>
           </tr>
           <tr>
             <td colspan="3">TAX 25%</td>
-            <td class="total">$</td>
+            <td >$</td>
           </tr>
           <tr>
-            <td colspan="3" class="grand total">GRAND TOTAL</td>
-            <td class="grand total">$</td>
+            <td colspan="3">GRAND TOTAL</td>
+            <td>$</td>
           </tr>
         </tbody>
       </table>
@@ -81,34 +174,4 @@ $this->load->view('inc/header', $data);
   </body>
 </html>
  
- 
-
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-
 <?php $this->load->view('inc/footer'); ?>
